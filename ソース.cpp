c732864d5e4aa1cebb587036d51d18e5271d2a65
Murasho7164 <87461873+Murasho7164@users.noreply.github.com>
2021-12-15 22:51:20 +0900

    Create ソース.cpp
    215A

---
+++ ソース.cpp
@@ -0,0 +1,16 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+using ll = long long;
+using P = pair<int, int>;
+#include<string>
+#include<vector>
+
+int main() {
+    //215
+    string s; cin >> s;
+    if(s=="Hello,World!") cout<<"AC"<<endl;
+    else cout << "WA" << endl;
+
+    return 0;
+}
