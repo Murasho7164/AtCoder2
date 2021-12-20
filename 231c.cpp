84b48356471735a011741370fc4fa2cf69434c90
Murasho7164 <87461873+Murasho7164@users.noreply.github.com>
2021-12-21 03:44:49 +0900

    Create 231c.cpp

---
+++ 231c.cpp
@@ -0,0 +1,30 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+    //231
+    int n, q; cin >> n >> q;
+    vector<int> a(n), x(q);
+    rep(i, n)cin >> a[i];
+    rep(i, q)cin >> x[i];
+
+    int count = 0;
+
+    sort(a.rbegin(), a.rend());
+
+    rep(i, q) {
+        count = 0;
+        rep(j, n) {
+            if (a[j] >= x[i]) {
+                count++;
+            }
+            else break;
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
