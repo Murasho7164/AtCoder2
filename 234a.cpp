7648ffeb58003110e6c56d0cd60fd141112941eb
Murasho7164 <87461873+Murasho7164@users.noreply.github.com>
2022-01-08 22:22:14 +0900

    a

--- 234a.cpp
+++ 234a.cpp
@@ -3,14 +3,14 @@ using namespace std;
 #define rep(i,n) for(int i=0;i<n;++i)
 using ll = long long;
 using P = pair<int, int>;
+int f(int x) {
+    return x * x + 2 * x + 3;
+}
 
 int main() {
-    //164
-    int s, w;
-    cin >> s >> w;
-
-    if (s > w)cout << "safe" << endl;
-    else cout << "unsafe" << endl;
+    //234
+    int t; cin >> t;
+    cout << f(f(f(t) + t) + f(f(t))) << endl;
 
     return 0;
 }
