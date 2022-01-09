c45c1fdd473328f2bbe1e39e4a1469e8660300a5
Murasho7164 <87461873+Murasho7164@users.noreply.github.com>
2022-01-09 13:47:28 +0900

    Create 198a.cpp

---
+++ 198a.cpp
@@ -0,0 +1,14 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+    //198
+    int n; cin >> n;
+
+    cout << n-1 << endl;
+
+    return 0;
+}
